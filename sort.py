f7bb596e54eb1869d9eb1acb25d603242b2a8fd4
Spksandy <90847173+Spksandy@users.noreply.github.com>
2022-03-03 16:42:25 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,12 @@
+import math
+a=int(input("Enter x1:"))
+b=int(input("Enter y1:"))
+c=int(input("Enter x2:"))
+maxm=max(a,b,c)
+minm=min(a,b,c)
+middle=(a+b+c)-maxm-minm
+print(maxm,middle,minm)
+
+
+
+
