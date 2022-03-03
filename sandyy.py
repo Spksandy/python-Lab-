1a0290078a474df3951cc701505a19a8f50c96a2
Spksandy <90847173+Spksandy@users.noreply.github.com>
2022-03-03 16:46:34 +0530

    Add files via upload

---
+++ sandyy.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a value:"))
+list=[1,5,6,4,9,2,3,8]
+for i in range(len(1ist)):
+	for j in range(1,i+1):
+		if(a[i]+a[j]==a):
+			print(a[0],a[i])
+			break
+		else:
+			print("Not found")
+
+                
