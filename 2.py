96804958900f040c88fa8ac29c57cb5d9791660c
Spksandy <90847173+Spksandy@users.noreply.github.com>
2022-05-04 12:30:17 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,4 +1,18 @@
-a=int(input("enter a number:"))
-b=a<<5
-print(b)
+# w.a.p to creat a function which will take a string containing name such as 'surendra kumr singh' as input and return a string which will contain a short name such as 's.k. singh' 
+
+'''def short(s):
+	a=s.split()
+	s1=''
+	for i in range(len(a)-1):
+		s1+=a[i][0]+'.'
+	s1+=' '+a[len(a)-1]
+	print(s1)
+	return s1
+s=input()
+s1=short(s)
+print(s1)s'''
+
+#w.a.p to take string frome user and make it a super reduce string by removing the same consicutive character.
+
+
 
